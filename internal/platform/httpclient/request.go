b3ce4c16565bf328ff6e6f28c372b50cf9ffc0f0
Caio Ferreira <caiorcferreira@gmail.com>
2020-12-23 16:04:01 -0300

    fix(httpclient): comment stdlib json parser

--- internal/platform/httpclient/request.go
+++ internal/platform/httpclient/request.go
@@ -2,7 +2,7 @@ package httpclient
 
 import (
 	"bytes"
-	"encoding/json"
+	//"encoding/json"
 	"github.com/b2wdigital/restQL-golang/v4/pkg/restql"
 	"github.com/pkg/errors"
 	"github.com/valyala/fasthttp"
